2133a050d51676b041a6e126e56d2716e6299ea0
Nick Stiegman <nick12@bu.edu>
2012-03-24 11:56:02 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BytelightServer::Application.config.secret_token = '27fc522f8dda78aebb266b0ece3db8c16a78f5aeb10c285d94ecc9df22e786c9ad558a615e7bfa4a6245a1208449d9be4c8302bdb4ab4b12896a854211fedcc2'
